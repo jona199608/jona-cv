6b94e79deb527711afffbe65a17a01c41758bcce
jona199608 <subjona@gmail.com>
2022-04-20 12:02:37 -0300

    modificado commit

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("show");
   });
   
+const $form = document.querySelector('#form')
+const $buttonMailito = document.querySelector('#mensajeria')
+
+$form.addEventListener('submit',handLeSubmit)
+
+function handLeSubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailito.setAttribute('href',`mailto:subjona@gmail.com?subject= email ${form.get('email')} asunto ${form.get('text')}&body=  ${form.get('enviar')}`)
+  $buttonMailito.click()
+}
+
+
   // ScrollReveal().reveal('.showcase');
   // ScrollReveal().reveal('.news-cards', { delay: 500 });
   // ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
